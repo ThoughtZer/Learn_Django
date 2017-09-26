89fe4ec963210e35f79f736ebfc15ef57f017869
zhanglm-person <zhanglm_work@163.com>
2017-09-26 14:12:21 +0800

    file_upload save_file

--- MyModel/views.py
+++ MyModel/views.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from models import *
 from django.http import HttpResponse, JsonResponse
+import time
+import os
+
+from myDjango.settings import MEDIA_ROOT
 
 
 # Create your views here.
@@ -27,3 +32,26 @@ def ArticleDetail(request, ArticleId):
     article_id = ArticleId
     article = Article.objects.get(id=article_id)
     return render(request, 'article.html', {'article': article})
+
+
+def Upload_File(request):
+    File = request.FILES.get('FileArgName', None)
+    filePath = saveFile(File)
+    file = FileUpload(file_name=File, file_path=filePath)
+    file.save()
+    return JsonResponse({'ret': 0, 'msg': 'success'})
+
+
+# 自己定义的写入文件的方法，而且改变文件名加上时间戳
+def saveFile(caseFile):
+    FileName = caseFile.name
+    timestamp = str(int(time.time()))
+    pos = FileName.rfind('.')
+    FileName = FileName[:pos] + '_' + timestamp + FileName[pos:]
+    destination = open(os.path.join(MEDIA_ROOT, FileName), 'wb+')
+    # UPLOAD_DIR也是一定路径常量，可以定义到settings中
+    for chunk in caseFile.chunks():
+        # 分块写入文件
+        destination.write(chunk)
+    destination.close()
+    return FileName
